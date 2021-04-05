895603cec4c3b21a2e4f3e1a36125112b857c885
tanjiarui <tanjiarui@hotmail.com>
2021-04-05 13:56:04 +0800

    test all components

--- assignment 4/test.sql
+++ assignment 4/test.sql
@@ -12,6 +12,22 @@ set
 	quantity = 50
 where
 	productid = 100;
+--test valid_code
+declare
+available boolean;
+begin
+	valid_code('strategy', 'AclnsKi6C0', available);
+	dbms_output.put_line(case available when true then 'true' else 'false' end);
+end;
+/
+--test after_discount
+declare
+result number;
+begin
+	after_discount(100, 20, result);
+	dbms_output.put_line(result);
+end;
+/
 --test order_payment
 begin
 	dbms_output.put_line(order_payment(105, 'strategy', 'AclnsKi6C0'));
